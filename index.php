347ddb735052bba895a40e32e4b9c641a0efa343
Santiago Nuñez <133709203+SantiNunez2003@users.noreply.github.com>
2024-11-10 11:15:18 -0300

    Acts Completas

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <h1>Guia de Ejercicios de PHP</h1>
     <br>
 
-    <h2>Ejercicio 4</h2><br>
+    <h2>Ejercicio 4</h2>
     <!-- Ejercicio 4 -->
     <?php 
         $num = rand(1,100);
@@ -26,7 +26,7 @@
     ?>
     <hr>
 
-    <h2>Ejercicio 5</h2><br>
+    <h2>Ejercicio 5</h2>
     <!-- Ejercicio 5 -->
     <?php 
         $int = 10;
@@ -50,7 +50,7 @@
     ?>
     <hr>
     
-    <h2>Ejercicio 6</h2><br>
+    <h2>Ejercicio 6</h2>
     <!-- Ejercicio 6 -->
     <?php 
         $precioIndividual = 5;
@@ -61,7 +61,7 @@
     ?>
     <hr>
 
-    <h2>Ejercicio 7</h2><br>
+    <h2>Ejercicio 7</h2>
     <!-- Ejercicio 7 -->
     <?php 
        $aleatorio = rand(1,3);
@@ -108,7 +108,7 @@
     ?>
     <hr>
 
-    <h2>Ejercicio 9</h2><br>
+    <h2>Ejercicio 9</h2>
     <!-- Ejercicio 9 -->
     <form method="post" action="">
         <label for="nombre">Ingrese su nombre</label>
@@ -131,9 +131,244 @@
                 echo "Es mayor de edad tiene: $edad años";
             } else {
                 echo "Es menor de edad tiene: $edad años";
-            }
+            };
         }
     ?>
+    <hr>
+
+    <h2>Ejercicio 10</h2>
+     <!-- Ejercicio 10 -->
+    <form method="post" action="">
+        <label for="nombre">Ingrese su nombre</label>
+        <input type="text" name="nombre">
+        <br>
+        Estudios:
+        <input type="radio" name="rad1" value="sin">Sin Estudios
+        <input type="radio" name="rad2" value="primario">Estudios Primarios
+        <input type="radio" name="rad3" value="secundario">Estudios Secundarios
+        <br>
+        <input type="sumbit" value="confirmar">
+    </form>
+    
+    <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $nombre = $_POST['nombre']; 
+            
+            echo("Hola $nombre, tus estudios son:" );
+
+            if ($_POST['rad1'] == "sin") {
+                echo "Sin estudios.";
+            };
+            if ($_POST['rad2'] == "primario") {
+                echo "Estudios primarios.";
+            };
+            if ($_POST['rad3'] == "secundario") {
+                echo "Estudios secundarios.";
+            };
+        }
+    ?>
+    <hr>
+
+    <h2>Ejercicio 11</h2>
+     <!-- Ejercicio 11 -->
+    <form method="post" action="">
+        <label for="nombre">Ingrese su nombre</label>
+        <input type="text" name="nombre">
+        <br>
+        Deportes:
+        <input type="checkbox" name="check1">Fútbol
+        <input type="checkbox" name="check2">Basquet
+        <input type="checkbox" name="check3">Tenis
+        <input type="checkbox" name="check4">Voley
+        <br>
+        <input type="sumbit" value="confirmar">
+    </form>
+
+    <?php 
+        $cantDeportes = 0;
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            
+            $nombre = $_POST['nombre']; 
+
+            if (isset($_POST['check1'])) {
+                $cantDeportes++;
+            };
+            if (isset($_POST['check2'])) {
+                $cantDeportes++;
+            };
+            if (isset($_POST['check3'])) {
+                $cantDeportes++;
+            };
+            if (isset($_POST['check4'])) {
+                $cantDeportes++;
+            };
+
+            echo("Hola $nombre, practicas: $cantDeportes deportes ");
+        }
+    ?>
+    <hr>
+
+    <h2>Ejercicio 12</h2>
+     <!-- Ejercicio 12 -->
+    <form method="post" action="">
+        <label for="nombre">Ingrese su nombre</label>
+        <input type="text" name="nombre">
+        <br>
+        Seleccione en que rango esta sus ingresos:
+        <select name="ingresos">
+        <option value="1">1-1000</option>
+        <option value="2">1001-3000</option>
+        <option value="3">>3000</option>
+        </select>
+        <input type="sumbit" value="confirmar">
+    </form>
+
+    <?php
+        echo $_POST['nombre'];
+        if ($_POST['ingresos'] == 3) {
+            echo " debe pagar impuestos.";
+        } else {
+            echo " no debe pagar impuestos a las ganancias.";
+        };
+    ?>
+    <hr>
+
+    <h2>Ejercicio 13</h2>
+     <!-- Ejercicio 13 -->
+     <form method="post" action="">
+        Completar contrato:<br>
+        <textarea name="contrato" rows="8" cols="80">
+            En la ciudad de [........], se acuerda entre la Empresa [..........] representada por el Sr. [..............] en su carácter de Apoderado, con domicilio en la calle [..............] y el Sr. [..............], futuro empleado con domicilio en [..............], celebrar el presente contrato a Plazo Fijo, de acuerdo a la normativa vigente de los artículos 90,92,93,94, 95 y concordantes de la Ley de Contrato de Trabajo N° 20.744.
+        </textarea>
+        <br>
+        <input type="sumbit" value="confirmar">
+    </form>
+
+    <?php  
+       echo $_POST['contrato'];
+    ?>
+    <hr>
+
+    <h2>Ejercicio 14</h2>
+     <!-- Ejercicio 14 -->
+    <?php
+        $diaSemana = array("Lunes", "Martes", "Miércoles", "Jueves","Viernes", "Sábado", "Domingo");
+        echo "Primer dia:" . $diaSemana[0] "<br>";
+        echo "Ultimo dia:" . $diaSemana[6];
+    ?>
+    <hr>
+
+    <h2>Ejercicio 15</h2>
+    <!-- Ejercicio 15 -->
+    <form method="post" action="">
+        <label for="nombre">Ingrese su nombre</label>
+        <input type="text" name="nombre">
+        <br>
+        Dirección:
+        <input type="text" name="direccion" size="40">
+        <br>
+        Jamon y Queso:
+        <input type="checkbox" name="jamonqueso">
+        <input type="text" name="cantjamonqueso" size="3">
+        <br>
+        Napolitana:
+        <input type="checkbox" name="napolitana">
+        <input type="text" name="cantnapolitana" size="3">
+        <br>
+        Muzzarella:
+        <input type="checkbox" name="muzzarella">
+        <input type="text" name="cantmuzzarella" size="3">
+        <br>
+        <input type="sumbit" value="confirmar">
+    </form>
+
+    <?php
+        $ar = fopen("datos.txt", "a") or
+            die("Problemas en la creacion");
+        fputs($ar, "Nombre:");
+        fputs($ar, $_REQUEST['nombre']);
+        fputs($ar, "\n");
+        fputs($ar, "Dirección:");
+        fputs($ar, $_REQUEST['direccion']);
+        fputs($ar, "\n");
+        if (isset($_REQUEST['jamonqueso'])) {
+            fputs($ar, "Cantidad de Jamón y Queso:");
+            fputs($ar, $_REQUEST['cantjamonqueso']);
+            fputs($ar, "\n");
+        }
+        if (isset($_REQUEST['napolitana'])) {
+            fputs($ar, "Cantidad de Napolitana:");
+            fputs($ar, $_REQUEST['cantnapolitana']);
+            fputs($ar, "\n");
+        }
+        if (isset($_REQUEST['muzzarella'])) {
+            fputs($ar, "Cantidad de Muzzarella:");
+            fputs($ar, $_REQUEST['cantmuzzarella']);
+            fputs($ar, "\n");
+        }
+
+        fputs($ar, "--------------------------------------------------------");
+        fputs($ar, "\n");
+        fclose($ar);
+        echo "El pedido se cargó correctamente.";
+    ?>
+    <hr>
+
+    <h2>Ejercicio 16</h2>
+    <!-- Ejercicio 16 -->
+    <?php
+        $ar = fopen("pedidos.txt", "r") or
+            die("No se pudo abrir el archivo");
+        while (!feof($ar)) {
+            $linea = fgets($ar);
+            $lineasalto = nl2br($linea);
+            echo $lineasalto;
+        }
+        fclose($ar);
+    ?>
+    <hr>
+
+    <h2>Ejercicio 16</h2>
+    <!-- Ejercicio 16 -->
+    <?php 
+        $usuarios = array(
+            'Javier' => 'claveJavier',
+            'Jose' => 'claveJose',
+            'Mariano' => 'claveMariano',
+            'Santiago' => 'claveSantiago',
+            'Joaquin' => 'claveJoaquin',
+        );
+
+        echo($registro['Santiago']);
+    
+    ?>
+    <hr>
+
+    <h2>Ejercicio 17</h2>
+    <!-- Ejercicio 17 -->
+    <form method="post" action="">
+        <label for="nombre">Ingrese su nombre</label>
+        <input type="text" name="nombre">
+        <br>
+        Ingrese su clave:
+        <input type="text" name="clave1" size="40">
+        <br>
+        Verifique su clave:
+        <input type="text" name="clave2">
+        <br>
+        <input type="sumbit" value="confirmar">
+    </form>
+
+    <?php
+        function verificarClaves($cla1, $cla2)
+        {
+            if ($cla1 != $cla2)
+            echo "Las claves ingresadas son distintas";
+        }
+
+        verificarClaves($_POST['clave1'], $_POST['clave2']);
+    ?>
+    <h2>Se verificaron las claves ingresadas.</h2>
 
 </body>
 </html>
